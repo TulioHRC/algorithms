3c18626240f0cb3f5cb1908951c03ef96e97f0b1
TulioHRC <grove1411@gmail.com>
2024-09-04 13:16:56 -0300

    test: error test

--- test/sortTest.cpp
+++ test/sortTest.cpp
@@ -9,7 +9,7 @@ uint32_t factorial(uint32_t number) {
 
 TEST_CASE("Factorials are computed", "[factorial]") {
     REQUIRE(factorial(1) == 1);
-    REQUIRE(factorial(2) == 2);
+    REQUIRE(factorial(2) == 3);
     REQUIRE(factorial(3) == 6);
     REQUIRE(factorial(10) == 3628800);
 }
