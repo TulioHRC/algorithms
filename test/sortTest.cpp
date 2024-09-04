684af442456e55721f5010000790f85b564f409d
TulioHRC <grove1411@gmail.com>
2024-09-04 13:06:39 -0300

    test: add github actions

--- test/sortTest.cpp
+++ test/sortTest.cpp
@@ -11,5 +11,5 @@ TEST_CASE("Factorials are computed", "[factorial]") {
     REQUIRE(factorial(1) == 1);
     REQUIRE(factorial(2) == 2);
     REQUIRE(factorial(3) == 6);
-    REQUIRE(factorial(10) == 3'628'800);
+    REQUIRE(factorial(10) == 3628800);
 }
